93b571af419281f9367937ab233dadabcc32c3b5
SNXWXH <snxwxh@gmail.com>
2025-03-04 17:10:26 +0900

    Deploy Next.js site to Pages / build (push) 빌드 오류 수정3

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.jsx',
+  themeConfig: './theme.config.js',
 });
 
-module.exports = withNextra;
+module.exports = withNextra({
+  output: 'export',
+});
 
 // If you have other Next.js configurations, you can pass them as the parameter:
 // module.exports = withNextra({ /* other next.js config */ })
